058ad726a8d65826e3a35315f900c564742b1107
huguodong <531035580@qq.com>
2023-03-29 08:25:02 +0800

    删除无用代码

--- api/SimpleAdmin/SimpleAdmin.Web.Core/Components/AuthComponent.cs
+++ api/SimpleAdmin/SimpleAdmin.Web.Core/Components/AuthComponent.cs
@@ -12,22 +12,8 @@ public sealed class AuthComponent : IServiceComponent
         // JWT配置
         services.AddJwt<JwtHandler>(enableGlobalAuthorize: true, jwtBearerConfigure: options =>
         {
-            // Configure the Authority to the expected value for
-            // the authentication provider. This ensures the token
-            // is appropriately validated.
-            //options.Authority = "Authority URL"; // TODO: Update URL
 
-            // We have to hook the OnMessageReceived event in order to
-            // allow the JWT authentication handler to read the access
-            // token from the query string when a WebSocket or 
-            // Server-Sent Events request comes in.
-
-            // Sending the access token in the query string is required due to
-            // a limitation in Browser APIs. We restrict it to only calls to the
-            // SignalR hub in this code.
-            // See https://docs.microsoft.com/aspnet/core/signalr/security#access-token-logging
-            // for more information about security considerations when using
-            // the query string to transmit the access token.
+            //signalr jwt配置
             options.Events = new JwtBearerEvents
             {
                 OnMessageReceived = context =>
