8e0c1d2cbbc2c23293c1164d11db3818b7f7859a
hugudong <hugudong@zxzyjs.com>
2023-07-03 15:25:48 +0800

    更新注释

--- api/SimpleAdmin/SimpleAdmin.Web.Entry/Program.cs
+++ api/SimpleAdmin/SimpleAdmin.Web.Entry/Program.cs
@@ -1,3 +1,17 @@
+Console.WriteLine(@"¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨[¨€¨€¨€¨[   ¨€¨€¨€¨[¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨[     ¨€¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨[   ¨€¨€¨€¨[¨€¨€¨[¨€¨€¨€¨[   ¨€¨€¨[     
+¨€¨€¨X¨T¨T¨T¨T¨a¨€¨€¨U¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨U¨€¨€¨X¨T¨T¨€¨€¨[¨€¨€¨U     ¨€¨€¨X¨T¨T¨T¨T¨a¨€¨€¨X¨T¨T¨€¨€¨[¨€¨€¨X¨T¨T¨€¨€¨[¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨U¨€¨€¨U¨€¨€¨€¨€¨[  ¨€¨€¨U     
+¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨U¨€¨€¨X¨€¨€¨€¨€¨X¨€¨€¨U¨€¨€¨€¨€¨€¨€¨X¨a¨€¨€¨U     ¨€¨€¨€¨€¨€¨[  ¨€¨€¨€¨€¨€¨€¨€¨U¨€¨€¨U  ¨€¨€¨U¨€¨€¨X¨€¨€¨€¨€¨X¨€¨€¨U¨€¨€¨U¨€¨€¨X¨€¨€¨[ ¨€¨€¨U     
+¨^¨T¨T¨T¨T¨€¨€¨U¨€¨€¨U¨€¨€¨U¨^¨€¨€¨X¨a¨€¨€¨U¨€¨€¨X¨T¨T¨T¨a ¨€¨€¨U     ¨€¨€¨X¨T¨T¨a  ¨€¨€¨X¨T¨T¨€¨€¨U¨€¨€¨U  ¨€¨€¨U¨€¨€¨U¨^¨€¨€¨X¨a¨€¨€¨U¨€¨€¨U¨€¨€¨U¨^¨€¨€¨[¨€¨€¨U     
+¨€¨€¨€¨€¨€¨€¨€¨U¨€¨€¨U¨€¨€¨U ¨^¨T¨a ¨€¨€¨U¨€¨€¨U     ¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨U  ¨€¨€¨U¨€¨€¨€¨€¨€¨€¨X¨a¨€¨€¨U ¨^¨T¨a ¨€¨€¨U¨€¨€¨U¨€¨€¨U ¨^¨€¨€¨€¨€¨U     
+¨^¨T¨T¨T¨T¨T¨T¨a¨^¨T¨a¨^¨T¨a     ¨^¨T¨a¨^¨T¨a     ¨^¨T¨T¨T¨T¨T¨T¨a¨^¨T¨T¨T¨T¨T¨T¨a¨^¨T¨a  ¨^¨T¨a¨^¨T¨T¨T¨T¨T¨a ¨^¨T¨a     ¨^¨T¨a¨^¨T¨a¨^¨T¨a  ¨^¨T¨T¨T¨a     
+                                                                                           
+ ¨€¨€¨€¨€¨€¨€¨[  ¨€¨€¨€¨€¨€¨€¨[    ¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨€¨€¨€¨€¨[  ¨€¨€¨[ ¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨[  ¨€¨€¨€¨€¨€¨€¨[ 
+¨€¨€¨X¨T¨T¨T¨€¨€¨[¨€¨€¨X¨T¨T¨T¨€¨€¨[¨€¨€¨[¨€¨€¨X¨T¨T¨T¨T¨a¨^¨T¨T¨T¨T¨€¨€¨[¨€¨€¨€¨U¨€¨€¨X¨T¨€¨€¨€¨€¨[¨^¨T¨T¨T¨T¨€¨€¨[¨€¨€¨X¨T¨T¨T¨T¨a¨€¨€¨X¨T¨T¨T¨T¨a¨€¨€¨X¨T¨T¨€¨€¨[¨€¨€¨X¨T¨€¨€¨€¨€¨[
+¨€¨€¨U   ¨€¨€¨U¨€¨€¨U   ¨€¨€¨U¨^¨T¨a¨€¨€¨€¨€¨€¨€¨€¨[ ¨€¨€¨€¨€¨€¨X¨a¨^¨€¨€¨U¨€¨€¨U¨€¨€¨X¨€¨€¨U ¨€¨€¨€¨€¨€¨X¨a¨€¨€¨€¨€¨€¨€¨€¨[¨€¨€¨€¨€¨€¨€¨€¨[¨^¨€¨€¨€¨€¨€¨X¨a¨€¨€¨U¨€¨€¨X¨€¨€¨U
+¨€¨€¨U¨{¨{ ¨€¨€¨U¨€¨€¨U¨{¨{ ¨€¨€¨U¨€¨€¨[¨^¨T¨T¨T¨T¨€¨€¨U ¨^¨T¨T¨T¨€¨€¨[ ¨€¨€¨U¨€¨€¨€¨€¨X¨a¨€¨€¨U ¨^¨T¨T¨T¨€¨€¨[¨^¨T¨T¨T¨T¨€¨€¨U¨^¨T¨T¨T¨T¨€¨€¨U¨€¨€¨X¨T¨T¨€¨€¨[¨€¨€¨€¨€¨X¨a¨€¨€¨U
+¨^¨€¨€¨€¨€¨€¨€¨X¨a¨^¨€¨€¨€¨€¨€¨€¨X¨a¨^¨T¨a¨€¨€¨€¨€¨€¨€¨€¨U¨€¨€¨€¨€¨€¨€¨X¨a ¨€¨€¨U¨^¨€¨€¨€¨€¨€¨€¨X¨a¨€¨€¨€¨€¨€¨€¨X¨a¨€¨€¨€¨€¨€¨€¨€¨U¨€¨€¨€¨€¨€¨€¨€¨U¨^¨€¨€¨€¨€¨€¨X¨a¨^¨€¨€¨€¨€¨€¨€¨X¨a
+ ¨^¨T¨T??¨T¨a  ¨^¨T¨T??¨T¨a    ¨^¨T¨T¨T¨T¨T¨T¨a¨^¨T¨T¨T¨T¨T¨a  ¨^¨T¨a ¨^¨T¨T¨T¨T¨T¨a ¨^¨T¨T¨T¨T¨T¨a ¨^¨T¨T¨T¨T¨T¨T¨a¨^¨T¨T¨T¨T¨T¨T¨a ¨^¨T¨T¨T¨T¨a  ¨^¨T¨T¨T¨T¨T¨a 
+                                                                                      ");
 Serve.Run(RunOptions.Default.ConfigureBuilder(builder =>
 {
     builder.WebHost.UseUrls(builder.Configuration["AppSettings:Urls"]);
