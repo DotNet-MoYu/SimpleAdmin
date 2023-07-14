10f3de9436fcf8d6b37f315448c37378c5d4c070
hugudong@zxzyjs.com <hugudong@zxzyjs.com>
2023-07-14 15:07:47 +0800

    更新代码

--- api/SimpleAdmin/SimpleAdmin.Web.Entry/Program.cs
+++ api/SimpleAdmin/SimpleAdmin.Web.Entry/Program.cs
@@ -1,6 +1,7 @@
 using System.Drawing;
 
 Console.WriteLine(@"源码地址: https://gitee.com/zxzyjs/SimpleAdmin");
+Console.WriteLine(@"演示地址: http://153.101.199.83:12802/login");
 Console.WriteLine(@"QQ:531035580");
 Console.WriteLine("没有花里胡哨，只有简单、稳定、灵活、高效");
 Colorful.Console.WriteAsciiAlternating("SimpleAdmin", new Colorful.FrequencyBasedColorAlternator(3, Color.Yellow, Color.GreenYellow));
