cade483dc45d13810b277c8f8c81255fdff93c39
少林寺驻北固山办事处大神父王喇嘛 <531035580@qq.com>
2025-01-14 10:02:35 +0800

    feat(s-form-item): ✨ s-from-item组件优化
    支持tooltip和popover弹框

--- web/src/components/Form/SFormItem/index.vue
+++ web/src/components/Form/SFormItem/index.vue
@@ -8,12 +8,28 @@
     <slot></slot>
     <template #label>
       <el-space :size="3" v-if="tooltip">
-        <el-tooltip effect="dark" :content="props.tooltip" placement="top">
+        <el-tooltip :effect="tooltip.effect" :placement="tooltip.placement" :raw-content="tooltip.rawContent" :hide-after="tooltip.hideAfter">
+          <template #content>
+            <!-- 插槽允许父组件传递自定义的 tooltip 内容 -->
+            <slot name="tooltip">{{ tooltip.content }}</slot>
+          </template>
           <svg-icon icon="uiw:question-circle-o"></svg-icon>
         </el-tooltip>
         {{ $attrs.label }}
       </el-space>
-      <span v-else-if="$attrs.label">{{ $attrs.label }}{{ formContext?.labelSuffix }}</span>
+      <el-space :size="3" v-else-if="popover">
+        <el-popover :width="popover.width" :placement="popover.placement" :trigger="popover.trigger" :title="popover.title">
+          <template #default>
+            <!-- 插槽允许父组件传递自定义的 tooltip 内容 -->
+            <slot name="popover">{{ popover.content }}</slot>
+          </template>
+          <template #reference>
+            <svg-icon icon="uiw:question-circle-o"></svg-icon>
+          </template>
+        </el-popover>
+        {{ $attrs.label }}
+      </el-space>
+      <span v-else>{{ $attrs.label }}{{ formContext?.labelSuffix }}</span>
     </template>
   </el-form-item>
 </template>
@@ -25,12 +41,62 @@ const formContext = inject(formContextKey, undefined); //表单实例
 const formItemRef = ref<FormItemContext>(); //表单Item实例
 
 interface Props {
-  tooltip?: string | undefined;
+  /*是否显示 tooltip*/
+  tooltip?: {
+    /*内容*/
+    content?: any;
+    /*content 中的内容是否作为 HTML 字符串处理*/
+    rawContent?: boolean;
+    /*tooltip 显示的位置*/
+    placement?:
+      | "top"
+      | "top-start"
+      | "top-end"
+      | "bottom"
+      | "bottom-start"
+      | "bottom-end"
+      | "left"
+      | "left-start"
+      | "left-end"
+      | "right"
+      | "right-start"
+      | "right-end";
+    /*tooltip 主题*/
+    effect?: "dark" | "light";
+    /*延迟关闭，单位毫秒*/
+    hideAfter?: number;
+  };
+  /* 是否显示 tooltip*/
+  popover?: {
+    /*出现位置*/
+    placement?:
+      | "top"
+      | "top-start"
+      | "top-end"
+      | "bottom"
+      | "bottom-start"
+      | "bottom-end"
+      | "left"
+      | "left-start"
+      | "left-end"
+      | "right"
+      | "right-start"
+      | "right-end";
+    /*标题*/
+    title: string;
+    /*宽度*/
+    width?: string;
+    /*触发方式*/
+    trigger?: "click" | "focus" | "hover" | "contextmenu";
+    /*内容*/
+    content?: any;
+  };
 }
 
 //默认值
-const props = withDefaults(defineProps<Props>(), {
-  tooltip: undefined
+withDefaults(defineProps<Props>(), {
+  tooltip: undefined,
+  popover: undefined
 });
 </script>
 
