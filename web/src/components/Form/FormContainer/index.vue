6d0eb5b44ac0c03de33a8f3129a19ab8063cf78a
少林寺驻北固山办事处大神父王喇嘛 <531035580@qq.com>
2025-08-01 17:03:11 +0800

    feat: ✨ 优化form-container组件

--- web/src/components/Form/FormContainer/index.vue
+++ web/src/components/Form/FormContainer/index.vue
@@ -1,44 +1,68 @@
-<!-- 
- * @Description: 表单容器组件
- * @Author: huguodong 
- * @Date: 2023-12-15 15:38:02
-!-->
 <template>
-  <!-- 抽屉 -->
+  <!-- 抽屉方式 -->
   <el-drawer v-if="globalStore.drawerForm" v-model="visible" :destroy-on-close="true" :size="formProps.formSize" v-bind="$attrs" @close="close">
-    <template v-for="slotKey in slotKeys" #[slotKey]> <slot :name="slotKey" /></template>
+    <!-- header 插槽 -->
+    <template #header>
+      <slot name="header" />
+    </template>
+
+    <!-- 动态插槽（除特殊插槽） -->
+    <template v-for="slotKey in filteredSlotKeys" #[slotKey]>
+      <slot :name="slotKey" />
+    </template>
+
+    <!-- footer 插槽 -->
+    <template #footer>
+      <slot name="footer" />
+    </template>
   </el-drawer>
-  <!-- 对话框 -->
-  <el-dialog v-else top="50px" :visible="visible" :destroy-on-close="true" draggable v-bind="$attrs" @close="close" :width="formProps.formSize">
-    <template v-for="slotKey in slotKeys" #[slotKey]> <slot :name="slotKey" /></template>
+
+  <!-- 对话框方式 -->
+  <el-dialog v-else top="50px" v-model="visible" :destroy-on-close="true" draggable v-bind="$attrs" :width="formProps.formSize" @close="close">
+    <!-- title 插槽 -->
+    <template #header>
+      <slot name="title" />
+    </template>
+
+    <!-- 动态插槽（除特殊插槽） -->
+    <template v-for="slotKey in filteredSlotKeys" #[slotKey]>
+      <slot :name="slotKey" />
+    </template>
+
+    <!-- footer 插槽 -->
+    <template #footer>
+      <slot name="footer" />
+    </template>
   </el-dialog>
 </template>
 
 <script setup lang="ts" name="FormContainer">
 import { useGlobalStore } from "@/stores/modules";
 
-const visible = ref(false); // 是否显示表单容器
-const globalStore = useGlobalStore(); // 全局状态管理
-const instance = getCurrentInstance(); // 当前实例
-const slotKeys = computed(() => Object.keys(instance?.slots || [])); // 插槽名称列表
-const emit = defineEmits({ close: null }); // 自定义事件
+const visible = ref(false);
+const globalStore = useGlobalStore();
+const emit = defineEmits(["close"]);
+
 const formProps = defineProps({
-  /** 表单大小 */
   formSize: {
     type: String,
     default: "600px"
   }
 });
 
-/** 关闭表单容器 */
+const instance = getCurrentInstance();
+
+/** 所有具名插槽键 */
+const slotKeys = computed(() => Object.keys(instance?.slots || {}));
+
+/** 过滤掉 header/footer/title 等固定插槽 */
+const filteredSlotKeys = computed(() => slotKeys.value.filter(k => !["header", "footer", "title"].includes(k)));
+
+/** 关闭弹窗 */
 function close() {
   visible.value = false;
   emit("close");
 }
 </script>
 
-<style lang="scss" scoped>
-:deep(.el-dialog__body) {
-  margin-top: 20px;
-}
-</style>
+<style scoped lang="scss"></style>
